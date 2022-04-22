a6fb6a1194b306672cd6b839b07326a8ee56aa4e
malikdobbs <malik-dobbs@hotmail.com>
2022-04-22 14:25:37 +0000

    Added products app, model and fixtures

---
+++ products/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+# This model contains the programmatic name and a more friendly looking 
+# name for the user on the front-end side for category name
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+# The first field is a foreign key to the category model.
+# We'll allow this to be null in the database and blank in forms
+# and if a category is deleted we'll set any products that use it to have null 
+# for this field rather than deleting the product.
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
