0e9801a9c0160072d8a0d35c6062824bc5aed831
malikdobbs <malik-dobbs@hotmail.com>
2022-04-30 15:12:13 +0000

    Added webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -77,12 +77,12 @@ form.addEventListener('submit', function(ev) {
         } else {
             // The payment has been processed
             if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
                 // Show a success message to your customer
                 // Theres a risk of the customer closing the window before
                 // callback execution. Set up a webhook or plugin to listen
                 // for the payment_intent.succeeded event that handles any
                 // business critical post payment actions
+                form.submit();
             }
         }
     });
